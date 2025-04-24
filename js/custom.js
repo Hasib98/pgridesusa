91f04c0016a0532459cab3cb59b0af089d798e0b
Hasib98 <phoenixoffire1998@gmail.com>
2025-04-24 22:05:00 +0600

    'final'

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("contact-link").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("contact-section").scrollIntoView({ behavior: "smooth" });
-});
-document.getElementById("services-link").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("services-section").scrollIntoView({ behavior: "smooth" });
-});
---
+++ main.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
