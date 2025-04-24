7a5bd08ea24079b704b8f03cb341230fb2311c0a
Hasib98 <phoenixoffire1998@gmail.com>
2025-04-25 00:45:40 +0600

    test

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   overlay.classList.toggle("active");
 });
+
+document
+  .getElementById("contact-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelector(".section_contact")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+
+document
+  .getElementById("services-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelector(".section_featured")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+document
+  .getElementById("about-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelector(".section_trusted")
+      .scrollIntoView({ behavior: "smooth" });
+  });
